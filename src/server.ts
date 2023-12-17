d6dc5e7525bef82cfaa004f9fc9854d729492b67
dhpe <dhpe@brasilcap.com.br>
2023-12-17 00:45:55 -0300

    Adicionado port environment

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 const users: { [key: string]: string } = {};
 
@@ -46,6 +47,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server running at http://localhost:3000');
 });
